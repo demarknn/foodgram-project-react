1f3521498d2905af45f91279b67c0336c4887283
Ivan comp <ivan.veselnikov@gmail.com>
2022-05-24 22:41:24 +0300

    ispr typeerrObMet new seril v3.5

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -29,9 +29,22 @@ class FullUserSerializer(DjoserUserSerializer):
             DjoserUserSerializer.Meta.read_only_fields) + ['is_subscribed']
 
 
-class RecipeFollowUserField(serializers.Field):
+class FollowUserField(serializers.Field):
     def get_attribute(self, instance):
         return Recipe.objects.filter(author=instance.following)
+
+    def to_representation(self, recipe_list):
+        recipe_data = []
+        for recipe in recipe_list:
+            recipe_data.append(
+                {
+                    "id": recipe.id,
+                    "name": recipe.name,
+                    "image": recipe.image.url,
+                    "cooking_time": recipe.cooking_time,
+                }
+            )
+        return recipe_data
 
 
 class FollowUsersSerializer(serializers.ModelSerializer):
@@ -41,7 +54,7 @@ class FollowUsersSerializer(serializers.ModelSerializer):
     first_name = serializers.ReadOnlyField(source='following.first_name')
     last_name = serializers.ReadOnlyField(source='following.last_name')
     is_subscribed = serializers.ReadOnlyField(default=True)
-    recipes = RecipeFollowUserField()
+    recipes = FollowUserField()
     recipes_count = serializers.IntegerField(
         source='following.count',
         read_only=True
