4c7ada6feac5ea1d38c4aa7a023518b6c03fc7eb
Ivan comp <ivan.veselnikov@gmail.com>
2022-05-24 23:23:07 +0300

    ispr typeerrObMet new seril v4.7

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -38,9 +38,9 @@ class TagSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
 
-    #tags = TagSerializer(read_only=True, many=True)
+    tags = TagSerializer(read_only=True, many=True)
     author = FullUserSerializer(read_only=True)
-    ingredients = IngredientAmountSerializer(required=True, many=True)
+    ingredients = IngredientAmountSerializer(many=True)
     is_favorited = serializers.SerializerMethodField()
     is_in_shopping_cart = serializers.SerializerMethodField()
 
