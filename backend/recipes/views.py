1e6802eeda1cd783a9943644663e63719d2962b3
Ivan comp <ivan.veselnikov@gmail.com>
2022-05-26 16:49:11 +0300

    fix fav v1.3

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -14,7 +14,7 @@ from .serializers import (
     IngredientSerializer, RecipeSerializer,
     TagSerializer, ShoppingListSerializer,
     FavouriteSerializer)
-from .filters import RecipeFilter, IngredientSearchFilter
+from .filters import IngredientSearchFilter  # RecipeFilter, 
 from .permissions import IsOwnerOrReadOnly
 
 
@@ -39,7 +39,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     filter_backends = [DjangoFilterBackend, ]
-    filterset_class = RecipeFilter
+    # filterset_class = RecipeFilter
     pagination_class = PageNumberPagination
     pagination_class.page_size = 6
     permission_classes = [IsOwnerOrReadOnly, ]
@@ -70,7 +70,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
 class ShoppingView(APIView):
     permission_classes = [permissions.IsAuthenticated, ]
-    filterset_class = RecipeFilter
+    # filterset_class = RecipeFilter
     pagination_class = None
 
     def post(self, request, pk):
